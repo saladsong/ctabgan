81e91581bb140f68390725fd388a74c438641cdd
lsw0504 <lsw0504@obzen.com>
2023-09-23 23:31:28 +0900

    Change: loss 계수조정

--- model/synthesizer/ctabgan_synthesizer.py
+++ model/synthesizer/ctabgan_synthesizer.py
@@ -1062,8 +1062,8 @@ class CTABGANSynthesizer:
                     loss_g_default
                     + loss_g_info
                     + loss_g_gen * 10
-                    + loss_g_jsd * 10
-                    + loss_g_cdiff
+                    + loss_g_jsd * 0.1
+                    + loss_g_cdiff * 0.1
                 )
                 loss_g.backward()
                 torch.nn.utils.clip_grad_norm_(self.generator.parameters(), 0.5)
